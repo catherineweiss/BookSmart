58b312da89ed04e65bce2db2ae5e6f3b3fb51f37
Stephen <ssli@sas.upenn.edu>
2019-11-24 02:22:20 -0500

    Create BorrowingTrends.js

---
+++ booksmart-app/client/src/views/booksmart/librarian/BorrowingTrends.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Bestsellers from "components/BorrowingTrends";
+
+function BorrowingTrendsPage() {
+    return (
+        <div className="App">
+            <BorrowingTrends/>
+        </div>
+    );
+}
+
+export default BorrowingTrendsPage;
