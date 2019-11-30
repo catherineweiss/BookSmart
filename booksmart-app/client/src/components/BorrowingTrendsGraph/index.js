5ca12617be6e5304d7810820a9c8d81748dc823f
Stephen <ssli@sas.upenn.edu>
2019-11-29 23:13:30 -0500

    Update index.js

--- booksmart-app/client/src/components/BorrowingTrendsGraph/index.js
+++ booksmart-app/client/src/components/BorrowingTrendsGraph/index.js
@@ -5,25 +5,12 @@ import C3Chart from 'react-c3js';
 import REACTDOM from "react-dom";
 
 class BorrowingTrendsGraph extends Component {
-	constructor(props) {
-		this.callAPI = this.callAPI.bind(this);
-		this.state = { jsonData : [] }
-	}
-
-	callAPI() {
-		const url = "/borrowingtrendsgraph/";
-
-        fetch(url)
-            .then(res => res.json())
-            .then(data => this.setState({ jsonData: data }))
-            .catch(err => err);
-	}
 
 	renderChart() {
         this.chart = c3.generate({
             bindto:"#chart1",
             data: {
-                columns: jsonData
+                columns: this.props.
             }
         });
     }
