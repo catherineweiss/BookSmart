fedb15edd359093cd1b263f080f30ae3b533a964
Donna St. Louis <donna3@wharton.upenn.edu>
2019-12-04 21:31:55 -0500

    Charts appear for both titles, but weird console errors abound

--- booksmart-app/client/src/components/BorrowingTrends/index.js
+++ booksmart-app/client/src/components/BorrowingTrends/index.js
@@ -1,17 +1,7 @@
 import React, { Component } from "react";
-// reactstrap components
-import {
-    FormGroup,
-    Input,
-    Button,
-    Container,
-    Row,
-    Col
-} from "reactstrap";
-import moment from "moment";
 import C3Chart from 'react-c3js';
-import 'c3/c3.css';
 import lodash from 'lodash';
+import 'c3/c3.css';
 
 class BorrowingTrends extends Component {
     constructor(props) {
@@ -19,53 +9,68 @@ class BorrowingTrends extends Component {
         this.callCheckoutsAPI = this.callCheckoutsAPI.bind(this);
         this.callRankingsAPI = this.callRankingsAPI.bind(this);
         this.callAPIs = this.callAPIs.bind(this);
+        this.chartifyCheckoutsData = this.chartifyCheckoutsData.bind(this);
         this.onChangeTitle = this.onChangeTitle.bind(this);
-        this.state = { borrowingTrends: {}, rankingTrends: [], title: 'harry potter and the sorcerer', validTitle: false, error: '' };
+        this.state = { borrowingTrends: {},
+                        rankingTrends: [],
+                        title: 'harry potter and the sorcerer',
+                        validTitle: false,
+                        loading: true,
+                        error: '' };
         this.callAPIs();
+    }
+
+    chartifyCheckoutsData(data) {
+      // Group records by ISBN
+      var dataByIsbn = lodash.groupBy(data, 'ISBN');
+      var yearData = [];
+      var checkoutsData = [];
+      // console.log(dataByIsbn);
+
+      for(let key in dataByIsbn) {
+          const value = dataByIsbn[key];
+          let checkoutsDataPerTitle = [];
+          checkoutsDataPerTitle.push(value[0].TITLE);
+          // console.log(checkoutsDataPerTitle)
+          for (let i in value) {
+            let record = value[i];
+            let month;
+            if (record.MONTH < 10) {
+              month = '0' + record.MONTH.toString();
+            } else {
+              month = record.MONTH.toString();
+            }
+            var dataRecord = record.YEAR.toString()+'-'+month+'-01';
+            // var date = new Date(dataRecord);
+            yearData.push(dataRecord);
+            checkoutsDataPerTitle.push(record.CHECKOUTS_PER_MONTH);
+          }
+          checkoutsData.push(checkoutsDataPerTitle)
+
+          yearData = lodash.uniq(yearData).sort()
+          yearData = ['x', ...yearData]
+          var borrowingTrends = {yearData, checkoutsData};
+      }
+      return borrowingTrends;
     }
 
     callCheckoutsAPI() {
         const title = this.state.title;
-        console.log(title);
+        // console.log(title);
         const url = "/borrowingtrends/" + title;
-
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
-
-                // Group records by ISBN
-                var dataByIsbn = lodash.groupBy(data, 'ISBN');
-                var yearData = [];
-                yearData.push('x');
-
-                var checkoutsData = [];
-                console.log(dataByIsbn);
-
-                for(let key in dataByIsbn) {
-                    const value = dataByIsbn[key];
-                    checkoutsData.push(value[0].TITLE);
-                    value.map(record => {
-                        var dataRecord =  record.YEAR+'-'+record.MONTH+'-01';
-                        yearData.push(dataRecord);
-                        checkoutsData.push(record.CHECKOUTS_PER_MONTH);
-                    });
-
-                    var borrowingTrends = {yearData, checkoutsData};
-                    console.log(borrowingTrends);
-                    this.setState({ borrowingTrends });
-                    break;
-                }
-
+                // console.log(data);
+                data = this.chartifyCheckoutsData(data);
+                this.setState({ borrowingTrends: data, loading: false });
             })
             .catch(err => err);
-
-
     }
 
     callRankingsAPI() {
         const title = this.state.title;
-        console.log(title);
+        // console.log(title);
         const url = "/nytrank/" + title;
 
         fetch(url)
@@ -93,36 +98,41 @@ class BorrowingTrends extends Component {
     }
 
     render() {
-        let borrowingTrendsResults;
-        let nytRankingResults;
-
+        let borrowingTrends = this.state.borrowingTrends;
+        let borrowingTrendsChart;
+        console.log(borrowingTrends);
         //creates the borrowingtrends graph
-       let columnData = [];
-           if(this.state.borrowingTrends) {
-               columnData.push(this.state.borrowingTrends.yearData);
-               columnData.push(this.state.borrowingTrends.checkoutsData);
-               const data = {
-                   x: 'x',
-    //        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
-                   columns: columnData
-               };
-               const axis = {
-                   x: {
-                       type: 'timeseries',
-                       tick: {
-                           format: '%Y-%m-%d'
-                       }
-                   }
-               };
-               return (
-                   <div>
-                       <C3Chart data={data} axis = {axis}/>
-                   </div>
-               );
+        let columnData = [];
+          if (this.state.loading) {
+            borrowingTrendsChart = <div>oh hello!</div>
+          } else {
+            columnData.push(borrowingTrends.yearData);
+            for (let i in borrowingTrends.checkoutsData) {
+              columnData.push(borrowingTrends.checkoutsData[i])
+            }
+            console.log(columnData);
+            const data = {
+                x: 'x',
+                xFormat: '%Y-%m-%d',
+                columns: columnData
+            };
+            const axis = {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                }
+            };
+            borrowingTrendsChart = <C3Chart data={data} axis={axis}/>
+          }
+          return (
+            <div>
+              {borrowingTrendsChart}
+            </div>
+          );
        }
-
     }
-}
 
 
 export default BorrowingTrends;
